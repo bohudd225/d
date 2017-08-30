9d0bccd060136c71778bb223ef0d8697ea413ff2
Francesco Cioria <firefelix@gmail.com>
2017-08-30 11:25:40 +0200

    improve initialization tests

--- test/initialization.test.ts
+++ test/initialization.test.ts
@@ -1,14 +1,41 @@
 import SocialAutofill from '../src/SocialAutofill';
 
+const validOptions = {
+  clientIds: { facebook: '123456' },
+  contacthub: () => {},
+  form: { fields: {} }
+}
+
+// tests for JS-runtime
 describe('Initialization', () => {
 
   it('should throw error if called without options', () => {
-    // test for JS-runtime
     expect(() => new SocialAutofill()).toThrow('"Social Autofill" must be initialized with some options')
   });
 
+  it('should throw error if called with invalid clientIds', () => {
+    const listOfInvalidClientIds = [
+      undefined,
+      null,
+      ['123'],
+      {
+        gugol: '12324'
+      },
+      {
+        facebook: 123
+      },
+      {
+        linkedin: {}
+      }
+    ];
+
+    listOfInvalidClientIds.forEach(clientIds => {
+      expect(() => new SocialAutofill({ ...validOptions, clientIds })).toThrow()
+    });
+  });
+
   it('should not throw any error if called with correct options', () => {
-    expect(() => new SocialAutofill({ clientIds: { facebook: '123456' }, form: { fields: {} } })).not.toThrow()
+    expect(() => new SocialAutofill(validOptions)).not.toThrow()
   });
 
 });
