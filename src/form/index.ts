9e2aaee9bfb13063e2233f7b9ce81bdf0901b4ad
Francesco Cioria <firefelix@gmail.com>
2017-08-30 17:34:48 +0200

    implement FormFiller class

--- src/form/index.ts
+++ src/form/index.ts
@@ -1,5 +1,45 @@
-import { FormOptions } from '../models';
+import { Fields, User } from '../models';
 
-export default function(formOptions: FormOptions) {
-  console.log(formOptions);
+type FieldName = keyof Fields;
+
+function getUserDataFromFieldName(fieldName: FieldName, user: User): string | undefined {
+  switch (fieldName) {
+    case 'firstName': return user.base.firstName
+    case 'lastName': return user.base.lastName
+    case 'email': return user.base.contacts.email
+    case 'gender': return user.base.gender
+    case 'dateOfBirth': return user.base.dob
+  }
+}
+
+export class FormFiller {
+
+  fields: Fields
+
+  constructor(fields: Fields) {
+    this.fields = fields;
+  }
+
+  fillFormWithUserData(user: User): void {
+    if (document.querySelector) {
+      const fields = this.fields;
+      const fieldNames = Object.keys(fields) as FieldName[];
+
+      fieldNames.forEach(fieldName => {
+        const userData = getUserDataFromFieldName(fieldName, user);
+
+        if (userData) {
+          const fieldSelector = fields[fieldName]!;
+          const element = document.querySelector(fieldSelector) as (HTMLInputElement | null);
+
+          if (element && typeof element !== 'undefined') {
+            element.value = userData;
+          } else {
+            console.error(`SocialAutofill: the element "${fieldSelector}" cannot be filled automatically because it is not a valid text field`);
+          }
+        }
+      });
+    }
+  }
+
 }
