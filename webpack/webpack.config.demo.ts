ec174aca69e1cb6a40befc40afb0fc6db394ecda
Francesco Cioria <firefelix@gmail.com>
2017-09-14 16:30:27 +0200

    fix: config variables should be evaluated as "undefined" in the HTML if missing

--- webpack/webpack.config.demo.ts
+++ webpack/webpack.config.demo.ts
@@ -5,6 +5,10 @@ import webpackBase, { paths } from './webpack.base';
 
 const config = JSON.parse(fs.existsSync(`${__dirname}/../config.json`) ? fs.readFileSync(`${__dirname}/../config.json`, 'utf8') : '{}');
 
+function getConfigVariable(value?: string): string {
+  return value ? `"${value}"` : 'undefined';
+}
+
 export default {
 
   ...webpackBase,
@@ -29,12 +33,12 @@ export default {
       sourceMap: true
     }),
     new HtmlWebpackPlugin({
-      CONTACTHUB_TOKEN: `"${config.CONTACTHUB_TOKEN || process.env.CONTACTHUB_TOKEN}"`,
-      CONTACTHUB_WORKSPACE_ID: `"${config.CONTACTHUB_WORKSPACE_ID || process.env.CONTACTHUB_WORKSPACE_ID}"`,
-      CONTACTHUB_NODE_ID: `"${config.CONTACTHUB_NODE_ID || process.env.CONTACTHUB_NODE_ID}"`,
-      FACEBOOK_CLIENT_ID: `"${config.FACEBOOK_CLIENT_ID || process.env.FACEBOOK_CLIENT_ID}"`,
-      GOOGLE_CLIENT_ID: `"${config.GOOGLE_CLIENT_ID || process.env.GOOGLE_CLIENT_ID}"`,
-      LINKEDIN_CLIENT_ID: `"${config.LINKEDIN_CLIENT_ID || process.env.LINKEDIN_CLIENT_ID}"`,
+      CONTACTHUB_TOKEN: getConfigVariable(config.CONTACTHUB_TOKEN || process.env.CONTACTHUB_TOKEN),
+      CONTACTHUB_WORKSPACE_ID: getConfigVariable(config.CONTACTHUB_WORKSPACE_ID || process.env.CONTACTHUB_WORKSPACE_ID),
+      CONTACTHUB_NODE_ID: getConfigVariable(config.CONTACTHUB_NODE_ID || process.env.CONTACTHUB_NODE_ID),
+      FACEBOOK_CLIENT_ID: getConfigVariable(config.FACEBOOK_CLIENT_ID || process.env.FACEBOOK_CLIENT_ID),
+      GOOGLE_CLIENT_ID: getConfigVariable(config.GOOGLE_CLIENT_ID || process.env.GOOGLE_CLIENT_ID),
+      LINKEDIN_CLIENT_ID: getConfigVariable(config.LINKEDIN_CLIENT_ID || process.env.LINKEDIN_CLIENT_ID),
       template: `${paths.DEMO}/index.ejs`
     })
   ]
